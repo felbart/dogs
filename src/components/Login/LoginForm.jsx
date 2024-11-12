915d9eb7dbea49b0629ca0eea197a85c78beeb1a
Felipe Leite <felbart@gmail.com>
2024-11-12 06:56:08 -0300

    login form

--- src/components/Login/LoginForm.jsx
+++ src/components/Login/LoginForm.jsx
@@ -1,6 +1,8 @@
 import { func } from 'prop-types';
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Input from '../Forms/Input';
+import Button from '../Forms/Button';
 
 const LoginForm = () => {
 
@@ -9,24 +11,27 @@ const LoginForm = () => {
 
     function handleSubmit(event) {
         event.preventDefaut();
-        fetch('')
+        fetch('https://dogsapi.origamid.dev/json/jwt-auth/token', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ username, password }),
+        }).then((response) => {
+          console.log(response);
+          return response.json();
+        }).then(json => {
+          console.log(json)
+        });
     }
 
   return (
     <section>
         <h1>Login</h1>
         <form action="" onSubmit={handleSubmit}>
-            <input
-            type="text"
-            value={username}
-            onChange={({ target }) => setUserName(target.value)}
-            />
-            <input
-            type="text"
-            value={password}
-            onChange={({ target }) => setPassword(target.value)}
-            />
-            <button>Entrar</button>
+          <Input label='Usuário' type='text' name='username' />
+          <Input label='Senha' type='password' name='password' />
+          <Button>Entrar</Button>
         </form>
         <Link to='/login/criar'>Cadastro</Link>
     </section>
