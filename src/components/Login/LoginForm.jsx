78b3e26badb11fe37ee0916c4f32dfacb2dcbc02
Felipe Leite <felbart@gmail.com>
2024-12-09 11:52:30 -0300

    validando forms

--- src/components/Login/LoginForm.jsx
+++ src/components/Login/LoginForm.jsx
@@ -3,34 +3,41 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import Input from '../Forms/Input';
 import Button from '../Forms/Button';
+import useForm from '../../Hooks/useForm';
 
 const LoginForm = () => {
 
-    const [ username, setUserName ] = React.useState('');
-    const [ password, setPassword ] = React.useState('');
+  const username = useForm();
+  const password = useForm();
 
-    function handleSubmit(event) {
+
+      function handleSubmit(event) {
         event.preventDefaut();
-        fetch('https://dogsapi.origamid.dev/json/jwt-auth/token', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ username, password }),
-        }).then((response) => {
-          console.log(response);
-          return response.json();
-        }).then(json => {
-          console.log(json)
-        });
+
+        if(username.validate() && password.validate()) {
+
+          fetch('https://dogsapi.origamid.dev/json/jwt-auth/token', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(),
+          }).then((response) => {
+            console.log(response);
+            return response.json();
+          }).then(json => {
+            console.log(json)
+          });
+          
+        }
     }
 
   return (
     <section>
         <h1>Login</h1>
         <form action="" onSubmit={handleSubmit}>
-          <Input label='Usuário' type='text' name='username' />
-          <Input label='Senha' type='password' name='password' />
+          <Input label='Usuário' type='text' name='username' {...username} />
+          <Input label='Senha' type='password' name='password' {...password} />
           <Button>Entrar</Button>
         </form>
         <Link to='/login/criar'>Cadastro</Link>
